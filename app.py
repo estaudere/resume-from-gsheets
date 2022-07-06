752c2e91623c9bbe436653a3286c5451fb67c3a3
neha <nehadesaraju@gmail.com>
2022-07-05 22:48:41 -0500

    fix pdf workflow and add main page

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, request, render_template, jsonify, url_for, send_file, send_from_directory
+from flask import Flask, request, render_template, redirect, send_from_directory
 import requests
-from weasyprint import HTML, CSS
+from flask_weasyprint import HTML, CSS, render_pdf
 from weasyprint.text.fonts import FontConfiguration
 
 # Support for gomix's 'front-end' and 'back-end' UI.
@@ -9,6 +9,16 @@ app = Flask(__name__, static_folder='public', template_folder='views')
 KEY = "AIzaSyAbcwyz3Ba-7WRyTcIw_X_UBumxYQbhyWA"
 
 host = "http://127.0.0.1:5000/"
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if request.method == 'POST':
+        url = request.form.get('url', "https://docs.google.com/spreadsheets/d/1GBRTwNO_GSldGLUBWlBlUB9W-y3S8sE2x023My2vagw/edit#gid=1210157874")
+        sheet_id = url.split('spreadsheets/d/')[1]
+        if '/' in sheet_id:
+            sheet_id = sheet_id.split('/')[0]
+        return redirect('/' + sheet_id + '/pdf')
+    return render_template('index.html')
 
 @app.route('/<id>')
 def resume(id):
@@ -26,13 +36,12 @@ def resume_pdf(id):
     experience = pull_sheet_data(id,"Experience")
     skills = pull_sheet_data(id, "Skills")
 
-    html_string = render_template('resume.html', intro=intro, education=education, experience=experience, skills=skills)
+    html_string = render_template('pdf_resume.html', intro=intro, education=education, experience=experience, skills=skills)
     html = HTML(string=html_string)
     font_config = FontConfiguration()
     css = CSS(filename='./public/pdf.css', font_config=font_config)
-    html.write_pdf('resume.pdf', stylesheets=[css])
-
-    return "hello"
+    
+    return render_pdf(html, stylesheets=[css])
 
 @app.route('/favicon.ico') 
 def favicon(): 
